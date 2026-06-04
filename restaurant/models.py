338e9259446c1c77d3db9fbe15c77d461b725c19
Sujeet D <sujeetds90@gmail.com>
2026-06-04 15:20:54 +0530

    adding database and 2 modals

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    # ID is automatically created by Django as an auto-incrementing primary key
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.Name} - {self.BookingDate}"
+
+
+class Menu(models.Model):
+    # ID is automatically created by Django as an auto-incrementing primary key
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.Title
